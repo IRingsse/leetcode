75e42011bde20f99ff48c69eff2439d8c3f7bad9
IRSF <knsf@qq.com>
2021-01-13 14:49:13 +0800

    第十四题

--- src/Question014.java
+++ src/Question014.java
@@ -15,7 +15,7 @@
  */
 public class Question014 {
     public static void main(String[] args) {
-        String[] str = {"cair", "car" };
+        String[] str = {"cair", "cairll" };
         System.out.println(longestCommonPrefix(str));
 
     }
